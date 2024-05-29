c04e2fac79aef5db528bcf057134cf42ec816f0f
KathyKZ <kathykate.sooksutum@gmail.com>
2024-05-29 15:35:39 +0700

    feat:react assignment components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import "./App.css";
 
-function App() {
+function Header() {
   return (
-    <div>
-      {/*
-        Todo: 1 ให้ Declare ตัว Component ย่อย 
-       เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <header> 
-      */}
-      <header>
+    <header>
         <nav>
           <div className="logo">🚀 XYZ CORP</div>
           <ul className="nav-list">
@@ -26,12 +21,12 @@ function App() {
           </ul>
         </nav>
       </header>
+  )
+}
 
-      {/*
-        Todo 2: ให้ Declare ตัว Component ย่อย
-        เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <main> 
-      */}
-      <main>
+function Main() {
+  return (
+    <main>
         <div className="company-detail-left">
           <h1>Welcome to XYZ Corp</h1>
           <p>Your Trusted Partner in Excellence</p>
@@ -60,12 +55,12 @@ function App() {
           <button className="learn-button">Learn More</button>
         </div>
       </main>
+  )
+}
 
-      {/*
-        Todo 3: ให้ Declare ตัว Component ย่อย
-        เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <section className="product-section"> 
-      */}
-      <section className="product-section">
+function Section() {
+  return (
+    <section className="product-section">
         <div className="product-section-container">
           <h2 className="product-featured-heading">Featured Products</h2>
           <div className="product-card-list">
@@ -76,16 +71,7 @@ function App() {
               />
               <h3>Premium Bluetooth Headphones</h3>
               <p className="product-price">2,400 Baht</p>
-              {/*
-                Todo 5: ให้ Declare ตัว Component Button เพื่อ Render ปุ่ม “Add to Cart” ที่ซ้ำกันอยู่
-                (จุดที่ 1)
-              */}
-              {/*
-                Todo 6: หลังกจากทำ Todo 5 เสร็จ
-                ให้เปลี่ยน Style ของปุ่ม “Add to Card” แก้ไข className ใน Component `Button` ที่สร้างขึ้นมา
-                จาก “add-to-cart-button” เป็น “add-to-cart-button-secondary”
-              */}
-              <button className="add-to-cart-button">Add to Cart</button>
+              <Button />
             </div>
             <div className="product-card">
               <img
@@ -94,8 +80,7 @@ function App() {
               />
               <h3>Classic Leather Watch</h3>
               <p className="product-price">4,000 Baht</p>
-              {/* Todo 5: (จุดที่ 2) */}
-              <button className="add-to-cart-button">Add to Cart</button>
+              <Button />
             </div>
             <div className="product-card">
               <img
@@ -104,20 +89,35 @@ function App() {
               />
               <h3>Organic Green Tea</h3>
               <p className="product-price">79.99 Baht</p>
-              {/* Todo 5: (จุดที่ 3) */}
-              <button className="add-to-cart-button">Add to Cart</button>
+              <Button />
             </div>
           </div>
         </div>
       </section>
+  )
+}
+function Button() {
+  return (
+    <button className="add-to-cart-button-secondary">Add to Cart</button>
+  )
+}
 
-      {/*
-        Todo 4: ให้ Declare ตัว Component ย่อย
-        เพื่อแบ่งสัดส่วนของ HTML Element ในส่วนของ <footer> 
-      */}
-      <footer>
+function Footer() {
+  return (
+    <footer>
         <p>&copy; 2023 Your Ecommerce Store. All rights reserved.</p>
       </footer>
+  )
+}
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Section />
+      <Footer />
     </div>
   );
 }
